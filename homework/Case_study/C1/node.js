fdffaa75f4aae820335af87a79406ed617489919
nvrqm <hung0964153699@gmail.com>
2024-11-11 19:05:21 +0700

    caseStudy_1

---
+++ homework/Case_study/C1/node.js
@@ -0,0 +1,28 @@
+let numbers = [""];
+let amount = +prompt("Enter amount of numbers:");
+if (amount > 2) {
+    if (amount <= 20) {
+        for (let i = 0; i < amount; i++) {
+            let number = +prompt("Enter " + (i + 1) + " number:");
+            numbers.push(number);
+        }
+    } else {
+        alert("Too much, smaller or equal to 20 pls");
+    }
+    console.log(`Before assesing: ${numbers}`);
+    let increasing = true;
+    let distanceUp = numbers[1] - numbers[0];
+    for (let i = 2; i < numbers.length; i++) {
+        if (numbers[i] - numbers[i-1] != distanceUp) {
+            increasing = false;
+            break;
+        }
+    }
+    if (increasing) {
+        console.log(`The array is increasing at a rate of ${distanceUp}`);
+    } else {
+        console.log("The array is not increasing");
+    }
+} else {
+    console.log("Can be determined");
+}
